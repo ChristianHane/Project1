48c404d8f34a6a9b8a31c86f01281a8730d64c04
James Curatalo <jfcurat@gmail.com>
2018-01-24 02:54:15 -0800

    Minor Clean-Up
    noticed a couple indents were off, fixed
    other minor spaces added/deleted

--- assets/javascript/seatgeekAPI.js
+++ assets/javascript/seatgeekAPI.js
@@ -1,5 +1,5 @@
 // seatGeek API action
-//when a sport type button is clicked...
+// when a sport type button is clicked...
 $('.sportButton').on('click', function(event) {
 
   event.preventDefault();
@@ -9,16 +9,16 @@ $('.sportButton').on('click', function(event) {
   var selectedTaxonomy = $(this).val();
   console.log('sportTaxonomy clicked = ' + selectedTaxonomy);
 
-  //var lat = '&lat=' + localStorage.getItem('lat'); //still need to deal w/ incorporating lat/lon for location
-  //var lon = '&lon=' + localStorage.getItem('lng');
+  // var lat = '&lat=' + localStorage.getItem('lat'); // still need to deal w/ incorporating lat/lon for location
+  // var lon = '&lon=' + localStorage.getItem('lng');
   var endpoint = 'https://api.seatgeek.com/2/events';
   var client_id = '?client_id=MTAyNzk5MDR8MTUxNTg4MDMzMi4wOA';
   var location = '&geoip=true' //change var location = lat/lon vars from storage
   var range = '&range=25mi'; //change to get user input - var range = '&range=' + rangeUserInput + miOrKm;
-  //var performerSelection = '&performers.slug=' + performerUserInput;
+  // var performerSelection = '&performers.slug=' + performerUserInput;
   var sportType = '&taxonomies.name=' + selectedTaxonomy;
 
-  var queryString = endpoint + client_id + location /* + lat + lon */ + range + sportType; //still need to incorporate lat/lon for location
+  var queryString = endpoint + client_id + location /* + lat + lon */ + range + sportType; // still need to incorporate lat/lon for location
   console.log('queryString = ' + queryString);
 
   // get seatGeek data
@@ -50,7 +50,7 @@ $('.sportButton').on('click', function(event) {
       resultBody.append('<p>' + 'Local Start Time: ' + eventTimeConverted + '</p>');
       resultBody.append('<p>' + 'Venue Name: ' + sportsEvents[i].venue.name + '</p>');
       resultBody.append('<p>' + 'Address: ' + sportsEvents[i].venue.address + '\, ' + sportsEvents[i].venue.extended_address + '</p>');
-      //resultBody.append('<p>' + 'Location lat: ' + sportsEvents[i].venue.location.lat + '\, lon: ' + sportsEvents[i].venue.location.lon + '</p>');
+      // resultBody.append('<p>' + 'Location lat: ' + sportsEvents[i].venue.location.lat + '\, lon: ' + sportsEvents[i].venue.location.lon + '</p>');
       resultBody.append('<p>' + 'Lowest Price: \$' + sportsEvents[i].stats.lowest_price + '</p>');
 
       var resultLink = $('<a>');
