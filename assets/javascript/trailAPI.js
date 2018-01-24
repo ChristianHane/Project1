2c8b50f8e9be9b8ed36a13e1e892f421a100dbf3
Christian <christian_hane@aol.com>
2018-01-23 23:38:17 -0800

    finished display results for trails page

--- assets/javascript/trailAPI.js
+++ assets/javascript/trailAPI.js
@@ -1,40 +1,3 @@
-// $("#search-butt").on("click", function(event) {
-//     event.preventDefault();
-//     var lat = localStorage.getItem("lat");
-//     var lng = localStorage.getItem("lng");
-//     var radius = $("#radius").val();
-
-//     console.log(lat);
-//     console.log(lng);
-//     var trailsURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + search + "&location=42.3675294,-71.186966&radius=5000&key=AIzaSyBBx-fTMyEih3dDWIEQIVOkSYPYT0G8Sss";
-//     $.ajax({
-//         url: trailsURL,
-//         method: 'GET',
-//         headers: {
-//             'X-Mashape-Key': 'kxaa71ARPxmshqwGoA6fOcXu8ChDp1RDh9XjsnIo1otQQoB3nF',
-//             'Accept': 'text/plain'
-//         }
-//     }).done(function(response) {
-//         console.log(response);
-//         console.log(response.places.length);
-//         var places = response.places;
-//         for(var i = 0; i < places.length; i++){
-//             var newItem = $("<p>");
-//             newItem.append("<p>" + places[i].city + "</p>");
-//             newItem.append("<p>" + places[i].name + "</p>");
-//             newItem.append("<p>" + places[i].activities[0].activity_type_name + "</p>");
-//             newItem.append("<p>" + places[i].activities[0].length + " miles</p>");
-//             newItem.append("<p>" + places[i].activities[0].description + "</p>");
-//             // newItem.append("<img>" + pla)
-//             newItem.append("<a href='" + places[i].activities[0].url + "'>more info</a>");
-//             $("#list-places").append(newItem);
-//             console.log(newItem);
-//         }
-//     });
-
-    
-// })
-
 $(".outdoors-option").on("click", function(event) {
 
     event.preventDefault();
@@ -43,7 +6,7 @@ $(".outdoors-option").on("click", function(event) {
 
     var pyrmont = new google.maps.LatLng(lat,lng)
     var query = $(this).val();
-    // $("#search").val();
+
     console.log("made it")
     service = new google.maps.places.PlacesService(document.getElementById("list-places"));
     service.textSearch({
@@ -54,12 +17,32 @@ $(".outdoors-option").on("click", function(event) {
         console.log(data);
         console.log(status);
         for(var i = 0; i < data.length; i++) {
-            var newDiv = $("div")
-            newDiv.append("<p>" + data[i].name + "</p>");
-            newDiv.append("<p>" + data[i].formatted_address + "</p>");
-            newDiv.append("<p>Rating " + data[i].rating + "</p>");
-            newDiv.append("<a target='_blank' href='https://www.google.com/maps/place/" + data[i].formatted_address + "'>learn more</a>");
-            $("#list-places").append(newDiv);
+            var result = $("<div>");
+            result.addClass("card col-sm-5 results-card");
+            
+            var header = $("<h5>");
+            header.addClass("event-title");
+            header.text(data[i].name);
+            
+            var p = $("<p>");
+            p.addClass("event-info");
+            p.text(data[i].formatted_address);
+
+            var p2 = $("<p>");
+            p2.addClass("event-info");
+            p2.text(data[i].rating);
+            
+            var link = $("<a>");
+            link.attr("href", "https://www.google.com/maps/place/" + data[i].name);
+            link.addClass("btn btn-primary");
+            link.html("More Info");
+
+            result.append(header);
+            result.append(p);
+            result.append(p2);
+            result.append(link);
+
+            $("#list-places").append(result);
         }
     });
 })
