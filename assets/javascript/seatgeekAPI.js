8ffbd78533d26b069a1e04b84ca904d47bd1b6a1
James Curatalo <jfcurat@gmail.com>
2018-01-24 02:47:36 -0800

    Sport Type Selection Buttons Working
    got the buttons for selecting sport taxonomy working
    still need to get lat/lon incorporated as the used location, still using IP for now

--- assets/javascript/seatgeekAPI.js
+++ assets/javascript/seatgeekAPI.js
@@ -1,75 +1,36 @@
-// seat geek API action
-/*
-notes about query string params:
-EVENTS Endpoint = https://api.seatgeek.com/2/events
-client_id=clientID, like API-key
-geoip=true is to use ip address geographic location, or zip code
-range= is to find results w/i ##mi radius. can use '##km' for km range. default is 30mi
-per_page= is results per page set to number used
-page=1 is page to show, it is 1-indexed. can change this to flip thru results
-performers.slug=name-of-band-or-team-etc is to search performer(s) - can be used multiple times & can be either .id= or .slug= style in same query
-venue.(keys from venues)
-taxonomies param uses same modifiers as taxonomies endpoint
-!!!sorting - review docs & add later - sort by date, etc. .asc/.desc for ascending/descending
-!!!filtering - review docs & add later - sort by price, etc.
-notes about returned data object: 'The Events Response Document' section has all details
-VENUES Endpoint = https://api.seatgeek.com/2/venues
-PERFORMERS Endpoint = https://api.seatgeek.com/2/performers
-can be used to get performer info. links to spotify/lastFM, etc.
-*/
+// seatGeek API action
+//when a sport type button is clicked...
+$('.sportButton').on('click', function(event) {
 
-// var rangeUserInput = 0; // get this val from an input box
-// var zipUserInput = 00000; // for zip code entry from input box
-// var miOrKm = ''; // mi or km for range units
-// var resultsPerPageUserInput = 0; // allow user to select how many results per page? or maybe just set a default to keep page formatting to a known value / ease of styling
-// var resultsPageNumber = 1; // start @ 1 then have some way to change later
-// var performerUserInput = ''; // implement a way to add multiple performers later, like the movie search classwork/giphy wall hw
-
-// $('searchButton').on('click', function(event) {
-//   event.preventDefault();
-
-//   // breaking out query string into it's constituent params
-//   var endpoint = 'https://api.seatgeek.com/2/events?'; // maybe turn into an array so that VENUES or PERFORMERS or EVENTS endpoint can be chosen
-//   var client_id = 'client_id=MTAyNzk5MDR8MTUxNTg4MDMzMi4wOA';
-//   var locationIP = '&geoip=true'; // need to add an if/else for possible zipCode use here. make IP address use default.
-//   var locationZip = '&geoip=' + zipUserInput; // pass location user input here for zipCode
-//   var range = '&range=' + rangeUserInput + miOrKm;
-//   var resultsPerPage = '&per_page=' + resultsPerPageUserInput;
-//   var pageNumber = '&page=' + resultsPageNumber;
-//   var performerSelection = '&performers.slug=' + performerUserInput;
-
-//   // query string concatenated
-//   var urlString = endpoint + client_id + (locationIP || locationZip) + range + resultsPerPage + pageNumber + performerSelection;
-// })
-
-// everything above here is nasty
-/*
-$('.sport-button').on('click', function(event) {
   event.preventDefault();
 
+  $('#sports-results-display-zone').empty();
+
+  var selectedTaxonomy = $(this).val();
+  console.log('sportTaxonomy clicked = ' + selectedTaxonomy);
+
+  //var lat = '&lat=' + localStorage.getItem('lat'); //still need to deal w/ incorporating lat/lon for location
+  //var lon = '&lon=' + localStorage.getItem('lng');
   var endpoint = 'https://api.seatgeek.com/2/events';
   var client_id = '?client_id=MTAyNzk5MDR8MTUxNTg4MDMzMi4wOA';
-  //var location = get lat/lon vars from firebase
+  var location = '&geoip=true' //change var location = lat/lon vars from storage
   var range = '&range=25mi'; //change to get user input - var range = '&range=' + rangeUserInput + miOrKm;
   //var performerSelection = '&performers.slug=' + performerUserInput;
-  var sportType = '&taxonomies.name=' + $(this).val();
-  console.log(sportType);
+  var sportType = '&taxonomies.name=' + selectedTaxonomy;
 
-  var queryString = endpoint + client_id + '&geoip=true' + range + sportType;
-  console.log(queryString);
-  // just to test actually posting stuff to sports.html page
-  // need to rework this to incorporate user input into queryString
-  // just using LA Kings tester query for now
-  // posting to page successfully - not pretty lookin though */
+  var queryString = endpoint + client_id + location /* + lat + lon */ + range + sportType; //still need to incorporate lat/lon for location
+  console.log('queryString = ' + queryString);
+
+  // get seatGeek data
   $.ajax({
-    // make this = urlString later
-    url: 'https://api.seatgeek.com/2/events?client_id=MTAyNzk5MDR8MTUxNTg4MDMzMi4wOA&geoip=true&range=25mi&per_page=25&page=1&taxonomies.name=tennis', //performers.slug=los-angeles-kings
+    url: queryString,
     method: 'GET',
   }).done(function(response) {
-    console.log(response);
+
     var sportsEvents = response.events;
     console.log(sportsEvents);
 
+    // loop thru events & make a card displaying important info for each event - venue, date/time, link to buy tix, etc.
     for(var i = 0; i < sportsEvents.length; i++) {
 
       var result = $('<div>');
@@ -79,10 +40,9 @@ $('.sport-button').on('click', function(event) {
       resultHeader.addClass = ('event-title');
       resultHeader.text(sportsEvents[i].title);
 
+      // get time of event & format using moment.js
       var eventTime = sportsEvents[i].datetime_local;
-      console.log(eventTime);
       var eventTimeConverted = moment(eventTime).format('hh:mma, MM-DD-YYYY');
-      console.log(eventTimeConverted);
 
       var resultBody = $('<p>');
       resultBody.addClass('event-info');
@@ -104,22 +64,9 @@ $('.sport-button').on('click', function(event) {
       result.append(resultLink);
 
       $('#sports-results-display-zone').append(result);
-
-    /* var sportsInfo = $('<p>');
-    // remove <br> later & just style w/ CSS
-    sportsInfo.append('<p>' + '<br>' + 'Event Type: ' + sportsEvents[i].type + '</p>');
-    sportsInfo.append('<p>' + 'Event: ' + sportsEvents[i].title + '</p>');
-    sportsInfo.append('<p>' + 'Local Start Time: ' + eventTimeConverted + '</p>');
-    sportsInfo.append('<p>' + 'Venue Name: ' + sportsEvents[i].venue.name + '</p>');
-    sportsInfo.append('<p>' + 'Address: ' + sportsEvents[i].venue.address + '\, ' + sportsEvents[i].venue.extended_address + '</p>');
-    sportsInfo.append('<p>' + 'Location lat: ' + sportsEvents[i].venue.location.lat + '\, lon: ' + sportsEvents[i].venue.location.lon + '</p>');
-    sportsInfo.append('<p>' + 'Lowest Price: \$' + sportsEvents[i].stats.lowest_price + '</p>');
-    sportsInfo.append('<a href="' + sportsEvents[i].url + '">Click Here to Buy Tickets on SeatGeek</a>');
-    $('#sports-results-display-zone').append(sportsInfo);
-    console.log(sportsInfo); */
   }
   });
-//});
+});
 
 $(document).ready(function() {
   $('#cityHeader').text(localStorage.getItem('location'))
